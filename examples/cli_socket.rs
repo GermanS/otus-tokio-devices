1e4878066d8cfaba00b5c11daf6988e198275583
GermanS <german.semenkov@gmail.com>
2025-03-27 15:23:52 +0300

    impl socket tui generator

--- examples/cli_socket.rs
+++ examples/cli_socket.rs
@@ -3,19 +3,145 @@ use std::{io::Write, net::TcpStream};
 use otus_tokio_devices::power::Power;
 use otus_tokio_devices::socket::Socket;
 
+use color_eyre::Result;
+use crossterm::event::{Event, EventStream, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use futures::{FutureExt, StreamExt};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::Rect,
+    widgets::{Block, Borders, Gauge},
+};
+
 #[tokio::main]
-async fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+async fn main() -> color_eyre::Result<()> {
+    let terminal = ratatui::init();
+    let result = App::new().run(terminal).await;
+    ratatui::restore();
+    result
+}
 
-    loop {
-        let power = rand::random::<u8>();
+#[derive(Debug)]
+pub struct App {
+    /// Is the application running?
+    running: bool,
+    // Event stream.
+    event_stream: EventStream,
+    level: f32,
 
-        let socket = Socket::new( Power::new(power as f32) );
+    tcp_stream: TcpStream,
+}
 
-        println!("Sent message: {}", socket);
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            running: bool::default(),
+            event_stream: EventStream::default(),
+            level: 1500.0,
+            tcp_stream: TcpStream::connect("localhost:8080").expect("Unable to connect"),
+        }
+    }
+}
 
-        stream.write_all(socket.to_string().as_bytes()).unwrap();
+impl App {
+    /// Construct a new instance of [`App`].
+    pub fn new() -> Self {
+        Self::default()
+    }
 
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    /// Run the application's main loop.
+    pub async fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        self.running = true;
+        while self.running {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_crossterm_events().await?;
+        }
+        Ok(())
+    }
+
+    /// Renders the user interface.
+    ///
+    /// This is where you add new widgets. See the following resources for more information:
+    /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
+    /// - <https://github.com/ratatui/ratatui/tree/master/examples>
+    fn draw(&mut self, frame: &mut Frame) {
+        // Создаем элемент Gauge (шкала)
+        let gauge = Gauge::default()
+            .block(Block::default().borders(Borders::ALL).title(
+                "Управление розеткой. Нажмите [\"+\"/\"-\"] для изменения значений. Esc - выход",
+            ))
+            .label(format!("Мощность {:.1} W из 2000 W", self.level))
+            .ratio(((self.level - 500.0) / 1500.0).into());
+
+        let area = Rect {
+            height: frame.area().height.saturating_sub(2),
+            ..frame.area()
+        };
+        frame.render_widget(gauge, area)
+    }
+
+    /// Reads the crossterm events and updates the state of [`App`].
+    async fn handle_crossterm_events(&mut self) -> Result<()> {
+        tokio::select! {
+            event = self.event_stream.next().fuse() => {
+                match event {
+                    Some(Ok(evt)) => {
+                        match evt {
+                            Event::Key(key)
+                                if key.kind == KeyEventKind::Press
+                                    => self.on_key_event(key),
+                            Event::Mouse(_) => {}
+                            Event::Resize(_, _) => {}
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            _ = tokio::time::sleep(tokio::time::Duration::from_millis(20)) => {
+                // Sleep for a short duration to avoid busy waiting.
+            }
+        }
+        Ok(())
+    }
+
+    /// Handles the key events and updates the state of [`App`].
+    fn on_key_event(&mut self, key: KeyEvent) {
+        match (key.modifiers, key.code) {
+            (_, KeyCode::Esc | KeyCode::Char('q'))
+            | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
+            // Add other key handlers here.
+            (_, KeyCode::Char('+')) => self.increase_level(),
+            (_, KeyCode::Char('-')) => self.decrease_level(),
+            _ => {}
+        }
+    }
+
+    /// Set running to false to quit the application.
+    fn quit(&mut self) {
+        self.running = false;
+    }
+
+    fn increase_level(&mut self) {
+        if self.level < 2000.0 {
+            self.level += 2.5;
+        }
+
+        self.notify()
+    }
+
+    fn decrease_level(&mut self) {
+        if self.level > 500.0 {
+            self.level -= 2.5;
+        }
+
+        self.notify()
+    }
+
+    fn notify(&mut self) {
+        let socket = Socket::new(Power::new(self.level));
+
+        self.tcp_stream
+            .write_all(socket.to_string().as_bytes())
+            .unwrap();
     }
 }
